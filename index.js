d5897e08e6d98fa6cf78abad647cd17c4bc00748
sleepyhood <smartosw@korea.ac.kr>
2021-10-18 00:26:16 +0900

    2021-10-18 part 12 jsonwebtoken

--- index.js
+++ index.js
@@ -2,8 +2,76 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const bodyParser = require("body-parser");
+const cookeParser = require("cookie-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// 이거 하나떄문에 json이 제대로 안된것
+app.use(bodyParser.json());
+app.use(cookeParser());
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURI, {
+    // useNewRelParser: true,
+    // useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify: true,
+  })
+  .then(() => console.log("mongoDB connected"))
+  .catch((err) => console.log(err));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  // 회원가입시 필요한 정보들을 client에서가져옴,
+  // 데이터 베이스에 넣어줌
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ sucess: false, err });
+    return res.status(200).json({
+      sucess: true,
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  // 요청된 이메일이 데이터베이스에 존재하는지
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user) {
+      return res.json({
+        loginSucess: false,
+        message: "이메일에 해당되는 유저가 없습니다.",
+      });
+    }
+
+    // 이미 있으면, 비번이 일치하는지 찾음
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSucess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+      // 비번도 똑같으면 토큰생성
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+
+        // 토큰을 어디에 저장한다?  => 쿠키냐 로컬스토리지냐 그것이문제로다
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSucess: true, userID: user._id });
+      });
+    });
+  });
 });
 
 app.listen(port, () => {
---
+++ models/User.js
@@ -0,0 +1,83 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const myPlaintextPassword = "s0//P4$$w0rD";
+const someOtherPlaintextPassword = "not_bacon";
+
+const jwt = require("jsonwebtoken");
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    trim: true, // 공백제거
+    unique: 1, // 중복값 없이
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  lastname: {
+    type: String,
+    maxlength: 50,
+  },
+  role: {
+    type: Number,
+    default: 0,
+  },
+  image: String,
+  token: {
+    type: String,
+  },
+  tokenExp: {
+    type: Number,
+  },
+});
+
+userSchema.pre("save", function (next) {
+  var user = this;
+
+  // 비번만 바꾸거나 만들때만 해시함수 적용
+  if (user.isModified("password")) {
+    // 비번 암호화
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        // Store hash in your password DB.
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  // plainPassword는 원본 비번, 암호화된 비번과의 대조가 필요
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function (cb) {
+  var user = this;
+  // jsonwebtoken으로 token 생성
+  var token = jwt.sign(user._id.toHexString(), "secretToken");
+  //   user._id + 'secretToken' = token
+  // => 'secretToken' -> user._id
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = { User };
